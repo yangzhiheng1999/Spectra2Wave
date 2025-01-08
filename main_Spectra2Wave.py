067eec38fbc37c088e49cba7300d65a7d2c76cf5
GNEH <1808823379@qq.com>
2025-01-08 20:05:54 +0800

    修正对象的方法，方法中必须有self

--- main_Spectra2Wave.py
+++ main_Spectra2Wave.py
@@ -8,7 +8,7 @@ python环境3.11.5
 波浪谱为JONSWAP谱
 References:
 [1] Mazzaretto O M, Menéndez M, Lobeto H. A global evaluation of the JONSWAP spectra suitability on coastal areas[J]. Ocean Engineering, 2022, 266: 112756.
-[2] https://ww2.mathworks.cn/matlabcentral/answers/58194-how-to-generate-a-time-signal-from-spectrum
+[2] https://ww2.mathworks.cn/matlabcentral/answers/58194-how-to-generate-a-time-signal-from-spectrum * 主要参考matlab代码
 [3] Goda Y. Random seas and design of maritime structures[M]. World scientific, 2010.
 
 '''
@@ -43,16 +43,16 @@ class class_wave:
     def time_series(self, time_s:np.array, frequence:np.array):
         frequence_step = frequence[1]-frequence[0]
         # 波高的幅值
-        amplititude = np.sqrt(2 * self.S * frequence_step)
+        amplititude = np.sqrt(2 * self.S * frequence_step)  # 一维数组
         # 波浪分量的数量
         wave_num = len(frequence)
         # 相位
-        phase = np.random.rand(wave_num) * 2 * np.pi
+        phase = np.random.rand(wave_num) * 2 * np.pi    # 一维数组
         # 圆频率
-        omega = frequence * 2 * np.pi
+        omega = frequence * 2 * np.pi   # 一维数组
 
         # 波浪分量
-        wave_componant = [[] for _ in range(wave_num)]
+        wave_componant = [[] for _ in range(wave_num)]  # 二维数组，每一行是一个波浪分量，每一列是一个时刻
         for i in range(wave_num):
             wave_componant[i] = amplititude[i] * np.cos(omega[i]*time_s + phase[i])
 
@@ -90,8 +90,8 @@ if __name__ == "__main__":
     frequence_step = 0.0001
     f = np.arange(frequence_step, frequence_length, frequence_step)
     # 时域时间
-    time_length = 360
-    time_step = 0.1
+    time_length = 3600
+    time_step = 1
     t_series = np.arange(0, time_length, time_step)
 
     # 创建波浪对象
